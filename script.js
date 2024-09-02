4cd7d6674cb9ac93f2ff769d881959760c163c03
Andrea Gulli <andreagulli11@gmail.com>
2024-09-02 11:55:18 +0200

    complete BONUS

--- script.js
+++ script.js
@@ -4,9 +4,10 @@
 let names = ['andrea', 'GIULIA', 'Ludovica'] 
 
 // Creo il secondo array
-let LowerNames = names.map(name => {
-    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
-})
+let LowerNames = names.map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
 
 console.log(LowerNames)
-e
+
+let UpperNames = names.map(name => name.charAt(0).toLowerCase() + name.slice(1).toUpperCase())
+console.log(UpperNames)
+
